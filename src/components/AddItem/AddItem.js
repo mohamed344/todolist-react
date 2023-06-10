f0c85267daa9ec934f0016d0b98ddc1e98426771
mohamed344 <mohamedyonko3@gmail.com>
2023-06-10 12:37:43 +0100

    loginApi

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,86 +1,85 @@
-import React from 'react'
-import './AddItem.css'
-import API from '../../api'
-import {TextField, Button, Stack} from '@mui/material'
+    import React from 'react'
+    import './AddItem.css'
+    import API from '../../api'
+    import {Button,TextField, Stack} from '@mui/material'
 
-const AddItem = ({input, setInput}) => {
+    const AddItem = ({input, setInput}) => {
 
-  const handleSubmit = async () => {
-    try {
-      const response = await API.post('/api/tasks/create', { title: input });
-      console.log(response.data);
-      setInput('');
-    } catch (error) {
-      console.error(error);
+    const handleSubmit = async () => {
+        try {
+        const response = await API.post('/api/users/register', { title: input });
+        console.log(response.data);
+        setInput('');
+        } catch (error) {
+        console.error(error);
+        }
+    };
+
+    // const updateTask = async(id) => {
+    //   try{
+    //     const response = await API.put(`/api/tasks/update/${id}`, { title: input });
+    //     console.log(response.data); 
+    //     setInput(response.data.title);
+    //       setTodos((prev) => [...prev, input]);
+    //       console.log('The task has been updated');
+    //   }catch(error){
+    //     console.log(error);
+    //   }
+    // }
+
+    const handleKeypress = (e) => {
+        if(e.key === 'Enter'){
+            handleSubmit();
+        }
     }
-  };
-
-
-  // const updateTask = async(id) => {
-  //   try{
-  //     const response = await API.put(`/api/tasks/update/${id}`, { title: input });
-  //     console.log(response.data); 
-  //     setInput(response.data.title);
-  //       setTodos((prev) => [...prev, input]);
-  //       console.log('The task has been updated');
-  //   }catch(error){
-  //     console.log(error);
-  //   }
-  // }
-
-  const handleKeypress = (e) => {
-    if(e.key === 'Enter'){
-      handleSubmit();
-    }
-  }
-    return (
-      <>
-          <Stack spacing={2} direction={'row'}>
-            <TextField
-                id="outlined-basic"
-                label="Enter your task"
-                color='success'
-                variant="outlined"
-                onKeyPress={handleKeypress}
-                onChange={(e) => {setInput(e.target.value)}}
+        return (
+        <>
+            <Stack spacing={2} direction={'row'}>
+                <TextField
+                    id="outlined-basic"
+                    label="Enter your task"
+                    color='success'
+                    variant="outlined"
+                    onKeyPress={handleKeypress}
+                    onChange={(e) => {setInput(e.target.value)}}
+                    sx={{
+                        borderTopLeftRadius: 15,
+                        borderBottomLeftRadius: 15,
+                        color: '#fff',
+                        width: '70%',
+                    }}
+                    InputProps={{
+                        style: {
+                        color: '#fff',
+                        fontSize: '16px',
+                        },
+                        classes: {
+                            root: 'success-root',
+                            focused: 'success-focused',
+                            notchedOutline: 'success-notchedOutline',
+                        },
+                    }}
+                    InputLabelProps={{
+                        style: {
+                        color: '#02943a',
+                        fontSize: '16px',
+                        },
+                    }}
+                />
+                {/* <input type='text' value={input} className='todoinpt' /> */}
+                {/* <button className='todobtn' disabled={input.length === 0}  onClick={handleSubmit}>Add a task</button> */}
+                <Button variant='contained'
                 sx={{
-                    borderTopLeftRadius: 15,
-                    borderBottomLeftRadius: 15,
-                    color: '#fff',
-                    width: '70%',
-                }}
-                InputProps={{
-                    style: {
-                      color: '#fff',
-                      fontSize: '16px',
+                    width: '30%',
+                    backgroundColor: '#02943a',
+                    '&:hover': {
+                        backgroundColor: '#35af64', // Hover background color
                     },
-                    classes: {
-                        root: 'success-root',
-                        focused: 'success-focused',
-                        notchedOutline: 'success-notchedOutline',
-                      },
-                }}
-                InputLabelProps={{
-                    style: {
-                      color: '#02943a',
-                      fontSize: '16px',
-                    },
-                }}
-            />
-            {/* <input type='text' value={input} className='todoinpt' /> */}
-            {/* <button className='todobtn' disabled={input.length === 0}  onClick={handleSubmit}>Add a task</button> */}
-            <Button variant='contained'
-            sx={{
-                width: '30%',
-                backgroundColor: '#02943a',
-                '&:hover': {
-                    backgroundColor: '#35af64', // Hover background color
-                },
-            }}>Add a task</Button>
-          </Stack>
-      </>
-      )
+                }}>Add a task</Button>
+            </Stack>
+        </>
+        )
 
-}
+    }
 
-export default AddItem;
+    export default AddItem;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,87 @@
-import React from 'react'
-import {Stack, Button, TextField, InputLabel} from "@mui/material"
-
+import React from "react";
+import { Stack, Button, TextField, InputLabel } from "@mui/material";
+import API from "../api";
 
 function Login() {
 
-    return (
-        <>
-            <Stack spacing={2}>
-                <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
-                <TextField id="outlined-basic" label="Enter your email" color='success' variant="outlined" />
-                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                <TextField
-                    id="standard-adornment-password"
-                    label="Enter your password"
-                    color='success'
-                    type={'password'}
-                />
-                <Button variant='contained' color='success'>Log In</Button>
-            </Stack>
-        </>
-    )
+    const handleLogin = async () => {
+        try {
+          const response = await API.post('/api/users/login', {
+            username: '',
+            password: ''
+          });
+          // Process the response, e.g., save authentication token, redirect to dashboard, etc.
+          console.log(response.data);
+        } catch (error) {
+          // Handle error, e.g., display error message, clear form fields, etc.
+          console.error(error);
+        }
+      };
+
+  return (
+    <Stack spacing={2}>
+      <InputLabel htmlFor="outlined-adornment-email" sx={{ color: "#DAD9D9" }}>
+        Email
+      </InputLabel>
+      <TextField
+        id="outlined-basic"
+        label="Enter your email"
+        color="success"
+        variant="outlined"
+        InputProps={{
+          style: {
+            color: "#fff",
+            fontSize: "16px",
+            width: "70%",
+          },
+          classes: {
+            root: "success-root",
+            focused: "success-focused",
+            notchedOutline: "success-notchedOutline",
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            color: "#DAD9D9",
+            fontSize: "16px",
+          },
+        }}
+      />
+      <InputLabel
+        htmlFor="outlined-adornment-password"
+        sx={{ color: "#DAD9D9" }}
+      >
+        Password
+      </InputLabel>
+      <TextField
+        id="standard-adornment-password"
+        label="Enter your password"
+        color="success"
+        type={"password"}
+        InputProps={{
+          style: {
+            color: "#fff",
+            fontSize: "16px",
+            width: "70%",
+          },
+          classes: {
+            root: "success-root",
+            focused: "success-focused",
+            notchedOutline: "success-notchedOutline",
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            color: "#DAD9D9",
+            fontSize: "16px",
+          },
+        }}
+      />
+      <Button variant="contained" color="success" sx={{ width: "70%" }} onClick={handleLogin}>
+        Log In
+      </Button>
+    </Stack>
+  );
 }
 
 export default Login;
